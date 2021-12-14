21ce3588cf9018cea38e4f47e8baa5ef218af656
MuchToKnow <justindc99edu@gmail.com>
2021-12-13 21:50:42 -0800

    init fe

---
+++ dfk-txns-fe/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+	  <h1 className="font-bold underline">
+	  	Hello World
+	  </h1>
+  )
+}
