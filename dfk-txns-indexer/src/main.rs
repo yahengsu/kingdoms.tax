71bacafbf8196930f7e5b488313c7600450177ea
NMTL <jk9988@students.cs.ubc.ca>
2021-12-23 18:22:24 -0800

    Indexer structure, abis, contracts, etc

--- dfk-txns-indexer/src/main.rs
+++ dfk-txns-indexer/src/main.rs
@@ -1,318 +1,122 @@
-use ethers::{core::
-    abi::{Abi, EventExt},
-    types::Address
-};
-use ethers::contract::Contract;
-use ethers::providers::{Provider, Http, Middleware};
-use ethers::types::{Filter, ValueOrArray};
-use std::convert::TryFrom;
 use anyhow::Result;
+use ethers::providers::{Http, Middleware, Provider};
+use ethers::types::{Filter, Log, ValueOrArray};
+use ethers::{
+    core::abi::{Abi, EventExt},
+    types::H160,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs;
+use std::{collections::HashMap, convert::TryFrom, str::FromStr};
+
+#[derive(Serialize, Deserialize)]
+struct AbiJson {
+    erc20: Abi,
+    erc721: Abi,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ContractJson {
+    erc20: HashMap<String, String>,
+    erc721: HashMap<String, String>,
+    other: HashMap<String, String>,
+}
+
+const BLOCKS_PER_REQ: i32 = 100;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = Provider::<Http>::try_from("https://api.s0.t.hmny.io").expect("Could not instantiate HTTP Provider");
-    let address = "0x72Cb10C6bfA5624dD07Ef608027E366bd690048F".parse::<Address>()?; // Jewel token for testing
-    let abi: Abi = serde_json::from_str(r#"[
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "name_",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "symbol_",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "value",
-        "type": "uint256"
-      }
-    ],
-    "name": "Approval",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "value",
-        "type": "uint256"
-      }
-    ],
-    "name": "Transfer",
-    "type": "event"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      }
-    ],
-    "name": "allowance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "approve",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
-      }
-    ],
-    "name": "balanceOf",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "decimals",
-    "outputs": [
-      {
-        "internalType": "uint8",
-        "name": "",
-        "type": "uint8"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "subtractedValue",
-        "type": "uint256"
-      }
-    ],
-    "name": "decreaseAllowance",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "addedValue",
-        "type": "uint256"
-      }
-    ],
-    "name": "increaseAllowance",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "name",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "symbol",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "totalSupply",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "recipient",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "transfer",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "sender",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "recipient",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "transferFrom",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-]"#)?;
+    let args: Vec<String> = env::args().collect();
 
-    let contract = Contract::new(address, abi, &provider);
-    let event = contract.abi().event("Transfer")?;
-    let filter = Filter::new().select(20758900..).event(&event.abi_signature()).address(ValueOrArray::Value(address));
-    let logs = provider.get_logs(&filter).await?;
-    for log in logs.iter() {
-        println!("txn hash: {:?}", log.transaction_hash);
-        let from = log.topics[1];
-        println!("txn from: {:?}", from);
-        let to = log.topics[2];
-        println!("txn to: {:?}", to);
-        let amt = log.data.to_string();
-        println!("amt: {}", amt);
+    // TODO: Instead of a manually set start block, get latest processed block from DB service.
+    let mut start_block = 20870000;
+    if args.len() > 1 && args[1] == "startBlock" {
+        start_block = args[2].parse::<i32>().expect(
+            "\n
+        Error reading command line args\n
+        Usage:\n
+        cargo run\n
+        cargo run -- --startBlock <number>\n
+        ",
+        );
     }
+
+    // Define paths to contract json
+    let contracts_path = "./constants/contracts.json";
+    let abi_path = "./constants/abis.json";
+
+    // Parse out contracts to handle
+    let contracts_data = fs::read_to_string(contracts_path).expect("unable to read contracts file");
+    let contracts: ContractJson =
+        serde_json::from_str(&contracts_data).expect("Couldn't read contracts json string");
+    let erc20s: Vec<H160> = contracts
+        .erc20
+        .into_values()
+        .map(|s| H160::from_str(&s).expect("Error converting addr string to H160"))
+        .collect();
+    let erc721s: Vec<H160> = contracts
+        .erc721
+        .into_values()
+        .map(|s| H160::from_str(&s).expect("Error converting addr string to H160"))
+        .collect();
+
+    // Parse out ABIs for contracts
+    let abi_data = fs::read_to_string(abi_path).expect("unable to read abi file");
+    let abis: AbiJson = serde_json::from_str(&abi_data).expect("Couldn't read abi json string");
+
+    // Activate harmony provider
+    let provider = Provider::<Http>::try_from("https://api.s0.t.hmny.io")
+        .expect("Could not instantiate HTTP Provider");
+
+    // Get transfer events from the ABIs
+    let erc20_transfer = abis.erc20.event("Transfer")?;
+    let erc721_transfer = abis.erc721.event("Transfer")?;
+
+    let erc20_filter = Filter::new()
+        .select(start_block..)
+        .event(&erc20_transfer.abi_signature())
+        .address(ValueOrArray::Array(erc20s));
+
+    let erc721_filter = Filter::new()
+        .select(start_block..)
+        .event(&erc721_transfer.abi_signature())
+        .address(ValueOrArray::Array(erc721s));
+
+    //TODO: Run this periodically, increasing filter blockrange by BLOCKS_PER_REQ until we reach current block.
+    index_txns_in_filter(erc20_filter, &provider, String::from("erc20")).await?;
+    index_txns_in_filter(erc721_filter, &provider, String::from("erc721")).await?;
     Ok(())
+}
+
+async fn index_txns_in_filter(
+    filter: Filter,
+    provider: &Provider<Http>,
+    abi_type: String,
+) -> Result<()> {
+    let logs = provider.get_logs(&filter).await?;
+    Ok(push_txns_to_mongo_service(format_logs(logs, abi_type)).await?)
+}
+
+async fn push_txns_to_mongo_service(logs: serde_json::Value) -> Result<()> {
+    //TODO: Push valid transactions to Rick's mongo service once it's ready
+    println!("{:?}", logs);
+    Ok(())
+}
+
+fn format_logs(logs: Vec<Log>, abi_type: String) -> serde_json::Value {
+    //TODO: Format these into json to send to Rick's mongo service
+    for log in logs.iter() {
+        println!("{:?}", log);
+        println!("txn hash: {:?}", log.transaction_hash);
+        println!("txn from: {:?}", log.topics[1]);
+        println!("txn to: {:?}", log.topics[2]);
+        if abi_type == "erc20" {
+            println!("amt: {}", log.data.to_string());
+        }
+        else if abi_type == "erc721" {
+            println!("id: {:?}", log.topics[3]);
+        }
+    }
+    serde_json::from_str("{}").expect("lol")
 }
