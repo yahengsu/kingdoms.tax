66b8bc763133f9ec7047864ea2e327eb44d9aa5a
NMTL <jk9988@students.cs.ubc.ca>
2022-01-01 13:24:19 -0800

    Add log index fiel

--- backend/models/transaction.go
+++ backend/models/transaction.go
@@ -3,9 +3,10 @@ package models
 // Transaction is the database representation of a single DFK transaction.
 type Transaction struct {
 	Account      string `json:"account"`
+	BlockNum     int    `json:"block_number"`
 	CounterParty string `json:"counterparty"`
 	Direction    string `json:"direction"`
-	BlockNum     int    `json:"block_number"`
+	LogIndex     string `json:"log_index"`
 	NetAmount    string `json:"net_amount"`
 	Timestamp    int    `json:"timestamp"`
 	TokenAddr    string `json:"token_addr"`
