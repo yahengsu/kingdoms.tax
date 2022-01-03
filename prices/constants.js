c520f5c4b8fff512eaedc3a4f9f1df40a5565d8b
YaHeng Su <yaheng.su1@gmail.com>
2022-01-03 02:38:28 -0700

    added scripts to get price feeds

---
+++ prices/constants.js
@@ -0,0 +1,116 @@
+module.exports = {
+  lp: {
+    JEWEL: {
+      ONE: {
+        address: "0xeb579ddcd49a7beb3f205c9ff6006bb6390f138f",
+        inverse: false,
+        decimals: 18,
+      },
+      DFKGOLD: {
+        address: "0x321eafb0aed358966a90513290de99763946a54b",
+        inverse: true,
+        decimals: 3,
+      },
+      DFKSHVAS: {
+        address: "0xb270556714136049b27485f1aa8089b10f6f7f57",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKTEARS: {
+        address: "0xc79245ba0248abe8a385d588c0a9d3db261b453c",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKSHIMMERSKIN: {
+        address: "0x46e81f6e157245caf138c4fb564d6db2711cebbb",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKGREGG: {
+        address: "0x4758f4d2097aa2bd8f575c345d64abecfd552e4b",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKBLOATER: {
+        address: "0xc41235202daa55064d69981b6de4b7947868bb45",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKSILVERFIN: {
+        address: "0x5898e31d82afdb1d65edefe0601714a60f461201",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKSWFTHSL: {
+        address: "0x7d65edc0e1449d13be1141ce29e5a984d04aa374",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKGLDVN: {
+        address: "0x399d025fe22203357da9a5c9ff9888b7c25cc899",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKRGWD: {
+        address: "0x2e7276584897a099D07b118FAd51ad8c169F01ee",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKRDLF: {
+        address: "0x0bc8b60bcc36f39d77922d554424e1944ec7ca61",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKREDGILL: {
+        address: "0xc5d215ff592bea454146135542e72e0ae520406f",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKDRKWD: {
+        address: "0x92ecb67043aac4b876cdf2a5f98b8aefafe7cfc6",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKAMBRTFY: {
+        address: "0x41003cd3a8684cc1ec877cd8590f49d17a0bab71",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKLANTERNEYE: {
+        address: "0xd0068dad1cbbab7412b53ecfd4f5782a18568147",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKRCKRT: {
+        address: "0x3d864c9cdc9d5ef3ef78ddfa0f1514f98313dc5c",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKSAILFISH: {
+        address: "0xc8ded2476fe5379b56cf1603ba0d278c5d7a3974",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKIRONSCALE: {
+        address: "0xaa01bd693f3638700a0946ab8452c97afb59b93e",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKSPIDRFRT: {
+        address: "0xd2239146b69bfe5dbfe78ad69c4b435365ddefd1",
+        inverse: true,
+        decimals: 0,
+      },
+      DFKBLUESTEM: {
+        address: "0x352c159860f2b89a9e36c7e9bf9a9a262cba0244",
+        inverse: false,
+        decimals: 0,
+      },
+      DFKMILKWEED: {
+        address: "0xb6a51bfeeeeafcf669e76ead260d3ae135257499",
+        inverse: false,
+        decimals: 0,
+      },
+    },
+  },
+};
---
+++ prices/index.js
@@ -0,0 +1,259 @@
+require("dotenv").config();
+const { ethers } = require("ethers");
+const { Client } = require("pg");
+const axios = require("axios");
+var fs = require("fs");
+var { graphql, buildSchema } = require("graphql");
+const constants = require("./constants");
+
+const LP_PAIRS = Object.entries(constants.lp.JEWEL);
+const JEWEL_USDC_PAIR_ADDRESS = "0xa1221a5bbea699f507cc00bdedea05b5d2e32eba";
+
+let totalNumTickData = 0;
+let totalNumAddedToDB = 0;
+
+async function getPairData(pair) {
+  const tokenName = pair[0];
+  const pairAddress = pair[1].address;
+  const inversed = pair[1].inverse;
+  let skip = 0;
+
+  var cleanedData = [];
+
+  while (true) {
+    const query = `
+    {
+      pairHourDatas(first: 1000 skip: ${skip} where: {pair: "${pairAddress}"}) {
+        hourStartUnix
+        reserve0
+        reserve1
+      }
+    }
+  `;
+    const res = await axios({
+      url: "http://graph3.defikingdoms.com/subgraphs/name/defikingdoms/dex",
+      method: "POST",
+      data: {
+        query: query,
+        variables: { skip },
+      },
+    });
+
+    const data = await res.data.data.pairHourDatas;
+    if (data.length == 0) {
+      break;
+    }
+    skip += data.length;
+
+    for (pairHourData of data) {
+      const reserve0 = Number.parseFloat(pairHourData.reserve0);
+      const reserve1 = Number.parseFloat(pairHourData.reserve1);
+      // JEWEL / token
+      const ratio = reserve0 / reserve1;
+      let jewelPrice;
+      let tokenPrice;
+
+      if (inversed) {
+        jewelPrice = 1 / ratio;
+        tokenPrice = ratio;
+      } else {
+        jewelPrice = ratio;
+        tokenPrice = 1 / ratio;
+      }
+      cleanedData.push({
+        timestamp: pairHourData.hourStartUnix,
+        JEWEL: jewelPrice,
+        [`${tokenName}`]: tokenPrice,
+      });
+    }
+  }
+  totalNumTickData += cleanedData.length;
+  fs.writeFileSync(
+    `JEWEL-${tokenName}.json`,
+    JSON.stringify(cleanedData, null, 2),
+    { encoding: "utf-8", flag: "w" }
+  );
+}
+
+async function getJewelUsdPrices() {
+  // get data every two days
+  const STEP = 86400 * 2;
+  const START_TIMESTAMP = 1629769481;
+  const COINGECKO_START_TIMESTAMP = 1632452603;
+  const END_TIMESTAMP = (new Date().getTime() / 1000).toFixed(0);
+
+  const query = `
+    {
+      pairHourDatas(where: {pair: "${JEWEL_USDC_PAIR_ADDRESS}", hourStartUnix_lt: 1632452603}) {
+        hourStartUnix
+        reserve0
+        reserve1
+      }
+    }
+  `;
+
+  const res = await axios({
+    url: "http://graph3.defikingdoms.com/subgraphs/name/defikingdoms/dex",
+    method: "POST",
+    data: {
+      query: query,
+    },
+  });
+
+  const dfkData = await res.data.data.pairHourDatas;
+  let jewelPrices = [];
+  let numCalls = 0;
+  for (pairHourData of dfkData) {
+    const timestamp = pairHourData.hourStartUnix;
+    if (timestamp > COINGECKO_START_TIMESTAMP) {
+      break;
+    }
+    const reserve0 = Number.parseFloat(pairHourData.reserve0);
+    const reserve1 = Number.parseFloat(pairHourData.reserve1);
+    const ratio = reserve0 / reserve1;
+    jewelPrices.push({
+      timestamp: timestamp,
+      JEWEL: 1 / ratio,
+      USD: ratio,
+    });
+  }
+
+  let CURRENT_TIMESTAMP = START_TIMESTAMP;
+  let STEP_TIMESTAMP = START_TIMESTAMP + STEP;
+  while (CURRENT_TIMESTAMP < END_TIMESTAMP) {
+    const url = `https://api.coingecko.com/api/v3/coins/harmony-shard-0/contract/0x72Cb10C6bfA5624dD07Ef608027E366bd690048F/market_chart/range?vs_currency=USD&from=${CURRENT_TIMESTAMP}&to=${STEP_TIMESTAMP}`;
+
+    const res = await axios.get(url);
+    const data = res.data;
+    if (data.prices.length) {
+      for (price of data.prices) {
+        jewelPrices.push({
+          timestamp: Math.trunc(price[0] / 1000),
+          JEWEL: price[1],
+          USD: 1 / price[1],
+        });
+      }
+    }
+    CURRENT_TIMESTAMP = STEP_TIMESTAMP;
+    STEP_TIMESTAMP += STEP;
+    numCalls += 1;
+    await new Promise((resolve) => setTimeout(resolve, 700));
+  }
+  console.log(`Made ${numCalls} calls to coingecko api`);
+  totalNumTickData += jewelPrices.length;
+  fs.writeFileSync(`JEWEL-USD.json`, JSON.stringify(jewelPrices, null, 2), {
+    encoding: "utf-8",
+    flag: "w",
+  });
+}
+
+// update token jsons with usd prices
+function updateTokenUSDPrices(pair) {
+  const tokenName = pair[0];
+  var JEWEL_USD_PRICES = JSON.parse(fs.readFileSync(`JEWEL-USD.json`, "utf-8"));
+  try {
+    var TOKEN_JEWEL_PRICES = JSON.parse(
+      fs.readFileSync(`JEWEL-${tokenName}.json`, "utf-8")
+    );
+  } catch (error) {
+    console.log(
+      `Error when opening JEWEL-${tokenName}.json for token ${tokenName} in updateTokenUSDPrices`,
+      error
+    );
+    return;
+  }
+
+  let usd_prices_index = 0;
+
+  for (let i = 0; i < TOKEN_JEWEL_PRICES.length; i++) {
+    const timestamp = TOKEN_JEWEL_PRICES[i].timestamp;
+    let otherTimestamp = JEWEL_USD_PRICES[usd_prices_index].timestamp;
+    while (otherTimestamp < timestamp) {
+      usd_prices_index += 1;
+      otherTimestamp = JEWEL_USD_PRICES[usd_prices_index].timestamp;
+    }
+    const jewelUSDPrice = JEWEL_USD_PRICES[usd_prices_index].JEWEL;
+    const tokenUSDPrice = jewelUSDPrice * TOKEN_JEWEL_PRICES[i].JEWEL;
+
+    TOKEN_JEWEL_PRICES[i].USD = tokenUSDPrice;
+  }
+
+  fs.writeFileSync(
+    `JEWEL-${tokenName}.json`,
+    JSON.stringify(TOKEN_JEWEL_PRICES, null, 2),
+    { encoding: "utf-8", flag: "w" }
+  );
+}
+
+async function addPairDataToDB(pair, client) {
+  const tokenName = pair[0];
+  try {
+    var pairData = JSON.parse(
+      fs.readFileSync(`JEWEL-${tokenName}.json`, "utf-8")
+    );
+  } catch (error) {
+    console.log(
+      `Error when parsing TOKEN_JEWEL_PRICES for token ${tokenName} in addPairDatatoDB`
+    );
+    return;
+  }
+
+  let addedRows = 0;
+  for (tick of pairData) {
+    const timestamp = tick.timestamp;
+    const usdPrice = tick.USD;
+    const query = {
+      text: "INSERT INTO Price (timestamp, token, price) VALUES($1, $2, $3) ON CONFLICT (timestamp, token) DO NOTHING",
+      values: [timestamp, tokenName, usdPrice],
+    };
+    try {
+      const resp = await client.query(query);
+      totalNumAddedToDB += resp.rowCount;
+    } catch (error) {
+      console.log(error.stack);
+    }
+  }
+}
+
+// RUN OCCASIONALLY TO UPDATE DATA
+async function run() {
+  // get JEWEL USD Prices from grqphql API + coingecko
+  console.log("Getting JEWEL USD Prices");
+  await getJewelUsdPrices();
+
+  // get pair data from graphql API
+  for (pair of LP_PAIRS) {
+    console.log(`Getting JEWEL-${pair[0]} prices`);
+    await getPairData(pair);
+  }
+
+  // append item usd prices
+  for (pair of LP_PAIRS) {
+    console.log(`Updating ${pair[0]} USD prices`);
+    await updateTokenUSDPrices(pair);
+  }
+
+  console.log(`Expecting ${totalNumTickData} rows in DB`);
+  const pgClient = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_URL,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+  });
+  console.log("Connecting to postgres db");
+  await pgClient.connect();
+  console.log("Connected to postgres db");
+  // add rows to database
+  console.log("Adding pair usd data to db");
+  for (pair of LP_PAIRS) {
+    console.log(`Adding ${pair[0]} to db`);
+    await addPairDataToDB(pair, pgClient);
+    console.log(`Finished adding ${pair[0]} to db`);
+  }
+  console.log("Finished adding pair usd data to db");
+  console.log(`Added ${totalNumAddedToDB} rows to db`);
+  console.log("Finished running all tasks");
+}
+
+run();
