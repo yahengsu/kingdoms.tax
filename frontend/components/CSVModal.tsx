3aa1eec8ebd0d9d01c985ba7be09ffe86bcd4485
NMTL <jk9988@students.cs.ubc.ca>
2022-01-03 20:44:38 -0800

    formatting for export modal and fix horizontal scroll

--- frontend/components/CSVModal.tsx
+++ frontend/components/CSVModal.tsx
@@ -124,8 +124,8 @@ const CSVModal: React.FC<ExportProps> = ({ ...props }) => {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Download Transactions as CSV</h3>
+                <div className="flex items-start justify-between p-10 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-2xl font-semibold">Download Transactions as CSV</h3>
                 </div>
                 <div className="relative p-5 flex-col flex items-center">
                   <ProgressBar current={progress} end={total} />
