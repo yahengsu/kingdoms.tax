e4a8cf0990a3619d6b52a94a0c3dae3ebe997caf
Yanchen Ma <yanchenm@gmail.com>
2022-01-03 23:22:32 -0500

    add tab titles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,8 @@ import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import { validateAddress } from '../utils/address';
+import Head from 'next/head';
+import React from 'react';
 
 type WalletFormValues = {
   address: string;
@@ -20,34 +22,39 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex flex-col font-default w-full min-h-screen items-center justify-center bg-gray-50">
-      <h1 className="font-bold text-7xl text-transparent text-center bg-clip-text bg-gradient-to-r from-cyan-500 to-green-500">
-        Explore your <br /> DFK transactions.
-      </h1>
-      <FormProvider {...formMethods}>
-        <form className="mt-12 w-3/4 md:w-1/2 lg:w-3/7 xl:w-1/3" onSubmit={formMethods.handleSubmit(onSubmit)}>
-          <div className="flex flex-row space-x-5">
-            <Input
-              formFieldName="address"
-              placeholder="Wallet Address"
-              autoComplete="off"
-              errorState={Boolean(formErrors.address)}
-              formRegisterOptions={{
-                required: {
-                  value: true,
-                  message: 'Please enter a wallet address.',
-                },
-                validate: (value) => validateAddress(value) || 'Please enter a valid wallet address.',
-              }}
-            />
-            <Button buttonText="Search" />
-          </div>
-          <div className="mt-10 text-red-600 text-center">
-            {formErrors.address?.message || String.fromCharCode(160)}
-          </div>
-        </form>
-      </FormProvider>
-    </div>
+    <>
+      <Head>
+        <title>Transactions | kingdoms.tax</title>
+      </Head>
+      <div className="flex flex-col font-default w-full min-h-screen items-center justify-center bg-gray-50">
+        <h1 className="font-bold text-7xl text-transparent text-center bg-clip-text bg-gradient-to-r from-cyan-500 to-green-500">
+          Explore your <br /> DFK transactions.
+        </h1>
+        <FormProvider {...formMethods}>
+          <form className="mt-12 w-3/4 md:w-1/2 lg:w-3/7 xl:w-1/3" onSubmit={formMethods.handleSubmit(onSubmit)}>
+            <div className="flex flex-row space-x-5">
+              <Input
+                formFieldName="address"
+                placeholder="Wallet Address"
+                autoComplete="off"
+                errorState={Boolean(formErrors.address)}
+                formRegisterOptions={{
+                  required: {
+                    value: true,
+                    message: 'Please enter a wallet address.',
+                  },
+                  validate: (value) => validateAddress(value) || 'Please enter a valid wallet address.',
+                }}
+              />
+              <Button buttonText="Search" />
+            </div>
+            <div className="mt-10 text-red-600 text-center">
+              {formErrors.address?.message || String.fromCharCode(160)}
+            </div>
+          </form>
+        </FormProvider>
+      </div>
+    </>
   );
 };
 
--- frontend/pages/txns/[address].tsx
+++ frontend/pages/txns/[address].tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
@@ -26,22 +27,27 @@ const Txns: NextPage = () => {
   }, [startDate]);
 
   return (
-    <div className="flex flex-col font-default w-full min-h-screen items-center bg-gray-50 py-10">
-      <h1 className="font-bold text-3xl">Your Transactions</h1>
-      <SearchFilterBar
-        address={address}
-        startDate={startDate}
-        endDate={endDate}
-        onStartDateChange={setStartDate}
-        onEndDateChange={setEndDate}
-        onDateReset={resetDates}
-      />
-      <Transactions
-        address={typeof address == 'string' ? address : ''}
-        startTime={startDate ? String(startDate.getTime() / 1000) : ''}
-        endTime={endDate ? String(endDate.getTime() / 1000) : ''}
-      />
-    </div>
+    <>
+      <Head>
+        <title>Transactions | kingdoms.tax</title>
+      </Head>
+      <div className="flex flex-col font-default w-full min-h-screen items-center bg-gray-50 py-10">
+        <h1 className="font-bold text-3xl">Your Transactions</h1>
+        <SearchFilterBar
+          address={address}
+          startDate={startDate}
+          endDate={endDate}
+          onStartDateChange={setStartDate}
+          onEndDateChange={setEndDate}
+          onDateReset={resetDates}
+        />
+        <Transactions
+          address={typeof address == 'string' ? address : ''}
+          startTime={startDate ? String(startDate.getTime() / 1000) : ''}
+          endTime={endDate ? String(endDate.getTime() / 1000) : ''}
+        />
+      </div>
+    </>
   );
 };
 
