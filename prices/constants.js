44aabc17705fa411acafd6403b79781b94627241
YaHeng Su <yaheng.su1@gmail.com>
2022-01-03 21:37:56 -0700

    update script with xJEWEL and new db schema

--- prices/constants.js
+++ prices/constants.js
@@ -1,113 +1,135 @@
 module.exports = {
   lp: {
     JEWEL: {
-      ONE: {
-        address: "0xeb579ddcd49a7beb3f205c9ff6006bb6390f138f",
+      XJEWEL: {
+        address: "0x06745211eafb0902986ea3ad426abd0829534539",
+        token: "0xa9ce83507d872c5e1273e745abcfda849daa654f",
         inverse: false,
         decimals: 18,
       },
       DFKGOLD: {
         address: "0x321eafb0aed358966a90513290de99763946a54b",
+        token: "0x3a4edcf3312f44ef027acfd8c21382a5259936e7",
         inverse: true,
         decimals: 3,
       },
       DFKSHVAS: {
         address: "0xb270556714136049b27485f1aa8089b10f6f7f57",
+        token: "0x66f5bfd910cd83d3766c4b39d13730c911b2d286",
         inverse: true,
         decimals: 0,
       },
       DFKTEARS: {
         address: "0xc79245ba0248abe8a385d588c0a9d3db261b453c",
+        token: "0x24ea0d436d3c2602fbfefbe6a16bbc304c963d04",
         inverse: true,
         decimals: 0,
       },
       DFKSHIMMERSKIN: {
         address: "0x46e81f6e157245caf138c4fb564d6db2711cebbb",
+        token: "0x372CaF681353758f985597A35266f7b330a2A44D",
         inverse: true,
         decimals: 0,
       },
       DFKGREGG: {
         address: "0x4758f4d2097aa2bd8f575c345d64abecfd552e4b",
+        token: "0x6d605303e9Ac53C59A3Da1ecE36C9660c7A71da5",
         inverse: false,
         decimals: 0,
       },
       DFKBLOATER: {
         address: "0xc41235202daa55064d69981b6de4b7947868bb45",
+        token: "0x78aED65A2Cc40C7D8B0dF1554Da60b38AD351432",
         inverse: false,
         decimals: 0,
       },
       DFKSILVERFIN: {
         address: "0x5898e31d82afdb1d65edefe0601714a60f461201",
+        token: "0x2493cfDAcc0f9c07240B5B1C4BE08c62b8eEff69",
         inverse: true,
         decimals: 0,
       },
       DFKSWFTHSL: {
         address: "0x7d65edc0e1449d13be1141ce29e5a984d04aa374",
+        token: "0xCdfFe898E687E941b124dfB7d24983266492eF1d",
         inverse: false,
         decimals: 0,
       },
       DFKGLDVN: {
         address: "0x399d025fe22203357da9a5c9ff9888b7c25cc899",
+        token: "0x600541aD6Ce0a8b5dae68f086D46361534D20E80",
         inverse: true,
         decimals: 0,
       },
       DFKRGWD: {
         address: "0x2e7276584897a099D07b118FAd51ad8c169F01ee",
+        token: "0x043F9bd9Bb17dFc90dE3D416422695Dd8fa44486",
         inverse: true,
         decimals: 0,
       },
       DFKRDLF: {
         address: "0x0bc8b60bcc36f39d77922d554424e1944ec7ca61",
+        token: "0x094243DfABfBB3E6F71814618ace53f07362a84c",
         inverse: true,
         decimals: 0,
       },
       DFKREDGILL: {
         address: "0xc5d215ff592bea454146135542e72e0ae520406f",
+        token: "0xc5891912718ccFFcC9732D1942cCD98d5934C2e1",
         inverse: false,
         decimals: 0,
       },
       DFKDRKWD: {
         address: "0x92ecb67043aac4b876cdf2a5f98b8aefafe7cfc6",
+        token: "0x68EA4640C5ce6cC0c9A1F17B7b882cB1cBEACcd7",
         inverse: true,
         decimals: 0,
       },
       DFKAMBRTFY: {
         address: "0x41003cd3a8684cc1ec877cd8590f49d17a0bab71",
+        token: "0x6e1bC01Cc52D165B357c42042cF608159A2B81c1",
         inverse: true,
         decimals: 0,
       },
       DFKLANTERNEYE: {
         address: "0xd0068dad1cbbab7412b53ecfd4f5782a18568147",
+        token: "0x8Bf4A0888451C6b5412bCaD3D9dA3DCf5c6CA7BE",
         inverse: false,
         decimals: 0,
       },
       DFKRCKRT: {
         address: "0x3d864c9cdc9d5ef3ef78ddfa0f1514f98313dc5c",
+        token: "0x6B10Ad6E3b99090De20bF9f95F960addC35eF3E2",
         inverse: true,
         decimals: 0,
       },
       DFKSAILFISH: {
         address: "0xc8ded2476fe5379b56cf1603ba0d278c5d7a3974",
+        token: "0xb80A07e13240C31ec6dc0B5D72Af79d461dA3A70",
         inverse: false,
         decimals: 0,
       },
       DFKIRONSCALE: {
         address: "0xaa01bd693f3638700a0946ab8452c97afb59b93e",
+        token: "0xe4Cfee5bF05CeF3418DA74CFB89727D8E4fEE9FA",
         inverse: false,
         decimals: 0,
       },
       DFKSPIDRFRT: {
         address: "0xd2239146b69bfe5dbfe78ad69c4b435365ddefd1",
+        token: "0x19B9F05cdE7A61ab7aae5b0ed91aA62FF51CF881",
         inverse: true,
         decimals: 0,
       },
       DFKBLUESTEM: {
         address: "0x352c159860f2b89a9e36c7e9bf9a9a262cba0244",
+        token: "0xAC5c49Ff7E813dE1947DC74bbb1720c353079ac9",
         inverse: false,
         decimals: 0,
       },
       DFKMILKWEED: {
         address: "0xb6a51bfeeeeafcf669e76ead260d3ae135257499",
+        token: "0xc0214b37FCD01511E6283Af5423CF24C96BB9808",
         inverse: false,
         decimals: 0,
       },
