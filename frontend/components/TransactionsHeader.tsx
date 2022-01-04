d0d5a2cd4fd59ce7e54b4a76c5f8914dbd1d4387
NMTL <jk9988@students.cs.ubc.ca>
2022-01-03 20:26:42 -0800

    fix header align

--- frontend/components/TransactionsHeader.tsx
+++ frontend/components/TransactionsHeader.tsx
@@ -5,11 +5,16 @@ const TransactionCard: React.FC = () => {
     <div className="grid grid-cols-12 w-2/3 text-xl font-semibold items-end">
       <p className="px-5 col-span-1">Txn</p>
       <p className="px-5 col-span-2">Date</p>
-      <p className="px-5 col-span-1">Direction</p>
+      <p className="px-5 col-span-1 justify-self-center">Direction</p>
       <p className="px-5 col-span-1">Token</p>
       <p className="px-5 col-span-2">Amount</p>
-      <p className="px-5 col-span-2">Type</p>
-      <p className="px-5 col-span-1" data-bs-toggle="tooltip" data-bs-placement="top" title="NFTs only">
+      <p className="px-5 col-span-2 justify-self-center">Type</p>
+      <p
+        className="px-5 col-span-1 justify-self-center"
+        data-bs-toggle="tooltip"
+        data-bs-placement="top"
+        title="NFTs only"
+      >
         TokenID
       </p>
       <p className="px-5 col-span-2">USD Value</p>
